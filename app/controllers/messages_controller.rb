6a45d2be3c5a205b03cfc1dd1911920e3b4e72db
ponhiroi <fmb.sgny.try.hy7@gmail.com>
2018-04-21 17:11:39 +0900

    respond_to

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,12 +8,11 @@ before_action :set_group
 
   def create
     @message = @group.messages.new(message_params)
-    respond_to do |format|
-      format.html { redirect_to group_messages_path(@group)}
-      format.json
-    end
     if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+      respond_to do |format|
+      format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
+      format.json
+      end
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
