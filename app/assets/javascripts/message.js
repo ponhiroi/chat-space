aec4e3a5a2e954c187f188147005f0dfef457c7b
ponhiroi <fmb.sgny.try.hy7@gmail.com>
2018-04-22 18:29:01 +0900

    java_script

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,27 @@
 $(function(){
+  function buildHTML(message){
+    var img = "";
+    if (message.image){
+      img = `<img src=${message.image} class: 'lower-message__image'>`
+    }
+    var html = `<div class="message">
+                  <ul class="upper-message">
+                    <li class="upper-message__user_name">
+                      ${message.user_name}
+                    </li>
+                    <li class="upper-message__date">
+                      ${message.time }
+                    </li>
+                  </ul>
+                  <ul class="lower-message">
+                      <li class="lower-message__content">
+                        ${ message.content }
+                      </li>
+                      ${ img }
+                  </ul>
+                </div>`
+    return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,5 +34,16 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      console.log(data)
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('.form__message').val('')
+      $('.messages.js-messages').animate({scrollTop: $('.messages')[0].scrollHeight},'fast')
+      $('.form__submit').prop("disabled", false)
+    })
+    .fail(function(){
+      alert('error');
+    })
   })
-})
+});
