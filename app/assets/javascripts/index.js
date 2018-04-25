65d86c1790ad8e1088ba3e6276158fcb552dbac4
ponhiroi <fmb.sgny.try.hy7@gmail.com>
2018-04-25 10:59:08 +0900

    add_user_remove

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,7 +1,6 @@
 $(function() {
 
-function appendUser(user){
-  console.log(user)
+function appendUser(user) {
   var html = `<div class="chat-group-user clearfix">
                 <p class="chat-group-user__name">${user.name}</p>
                 <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
@@ -28,7 +27,7 @@ function appendUser(user){
       if (users.length !== 0) {
         users.forEach(function(user){
           appendUser(user);
-       });
+      });
       }
       else {
         appendNoUser("一致するユーザーはいません");
@@ -38,4 +37,26 @@ function appendUser(user){
       alert('ユーザー検索に失敗しました');
     });
   });
+
+    function appendUserRemove(user_id, user_name) {
+      var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${ user_id }'>
+                    <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
+                    <p class='chat-group-user__name'>${ user_name }</p>
+                    <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                  </div>`
+      $("#chat-group-users").append(html);
+  };
+
+  $('#user-search-result').on('click', '.chat-group-user__btn--add', function(){
+    var user_id = $(this).attr('data-user-id');
+    var user_name = $(this).attr('data-user-name');
+  $(this).parent().remove();
+    appendUserRemove(user_id, user_name);
+  });
+
+  $('#chat-group-users').on('click', '.chat-group-user__btn--remove', function(){
+    $(this).parent().remove();
+  });
 });
+
+
