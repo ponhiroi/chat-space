9b4cdb495ecc33d2814005e47cf4926370a42037
ponhiroi <fmb.sgny.try.hy7@gmail.com>
2018-04-26 14:51:57 +0900

    delete_file

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :message do
     content Faker::Lorem.sentence
-    image File.open("#{Rails.root}/public/uploads/message/image/12/長友佑都.jpg")
+
     user
     group
   end
